ac39e3116b7f5a197c34cee99ca300ca066ae742
Gabriel Moreira <gabrielmoreira@gmail.com>
2012-01-10 23:16:30 -0200

    Pequenas mudanças. Transformar fieldName em FIELD_NAME, e objectName em OBJECT_NAME.

--- src/test/java/com/validator/FieldsTest.java
+++ src/test/java/com/validator/FieldsTest.java
@@ -21,8 +21,8 @@ import com.validator.validatable.StringValidatable;
 
 public class FieldsTest {
 
-	private final String fieldName = "MyFieldName";
-	private final String objectName = "MyObjectName";
+	private static final String FIELD_NAME = "MyFieldName";
+	private static final String OBJECT_NAME = "MyObjectName";
 
 	@Test
 	public void instanceTest() {
@@ -33,136 +33,136 @@ public class FieldsTest {
 	@Test
 	public void numberBigDecimalTest() {
 		BigDecimal fieldValue = new BigDecimal(10);
-		BigDecimalValidatable<?> validatable = number(fieldName, fieldValue);
-		assertEquals(fieldName, validatable.getFieldName());
+		BigDecimalValidatable<?> validatable = number(FIELD_NAME, fieldValue);
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void numberBigDecimalTest2() {
 		BigDecimal fieldValue = new BigDecimal(10);
-		BigDecimalValidatable<?> validatable = number(objectName, fieldName, fieldValue);
-		assertEquals(objectName, validatable.getObjectName());
-		assertEquals(fieldName, validatable.getFieldName());
+		BigDecimalValidatable<?> validatable = number(OBJECT_NAME, FIELD_NAME, fieldValue);
+		assertEquals(OBJECT_NAME, validatable.getObjectName());
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void numberByteTest() {
 		Byte fieldValue = new Byte((byte) 10);
-		ByteValidatable<?> validatable = number(fieldName, fieldValue);
-		assertEquals(fieldName, validatable.getFieldName());
+		ByteValidatable<?> validatable = number(FIELD_NAME, fieldValue);
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void numberByteTest2() {
 		Byte fieldValue = new Byte((byte) 10);
-		ByteValidatable<?> validatable = number(objectName, fieldName, fieldValue);
-		assertEquals(objectName, validatable.getObjectName());
-		assertEquals(fieldName, validatable.getFieldName());
+		ByteValidatable<?> validatable = number(OBJECT_NAME, FIELD_NAME, fieldValue);
+		assertEquals(OBJECT_NAME, validatable.getObjectName());
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void numberDoubleTest() {
 		Double fieldValue = new Double(10.0D);
-		DoubleValidatable<?> validatable = number(fieldName, fieldValue);
-		assertEquals(fieldName, validatable.getFieldName());
+		DoubleValidatable<?> validatable = number(FIELD_NAME, fieldValue);
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void numberDoubleTest2() {
 		Double fieldValue = new Double(10.0D);
-		DoubleValidatable<?> validatable = number(objectName, fieldName, fieldValue);
-		assertEquals(objectName, validatable.getObjectName());
-		assertEquals(fieldName, validatable.getFieldName());
+		DoubleValidatable<?> validatable = number(OBJECT_NAME, FIELD_NAME, fieldValue);
+		assertEquals(OBJECT_NAME, validatable.getObjectName());
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void numberIntegerTest() {
 		Integer fieldValue = new Integer(10);
-		IntegerValidatable<?> validatable = number(fieldName, fieldValue);
-		assertEquals(fieldName, validatable.getFieldName());
+		IntegerValidatable<?> validatable = number(FIELD_NAME, fieldValue);
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void numberIntegerTest2() {
 		Integer fieldValue = new Integer(10);
-		IntegerValidatable<?> validatable = number(objectName, fieldName, fieldValue);
-		assertEquals(objectName, validatable.getObjectName());
-		assertEquals(fieldName, validatable.getFieldName());
+		IntegerValidatable<?> validatable = number(OBJECT_NAME, FIELD_NAME, fieldValue);
+		assertEquals(OBJECT_NAME, validatable.getObjectName());
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void numberLongTest() {
 		Long fieldValue = new Long(10);
-		LongValidatable<?> validatable = number(fieldName, fieldValue);
-		assertEquals(fieldName, validatable.getFieldName());
+		LongValidatable<?> validatable = number(FIELD_NAME, fieldValue);
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void numberLongTest2() {
 		Long fieldValue = new Long(10);
-		LongValidatable<?> validatable = number(objectName, fieldName, fieldValue);
-		assertEquals(objectName, validatable.getObjectName());
-		assertEquals(fieldName, validatable.getFieldName());
+		LongValidatable<?> validatable = number(OBJECT_NAME, FIELD_NAME, fieldValue);
+		assertEquals(OBJECT_NAME, validatable.getObjectName());
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void numberShortTest() {
 		Short fieldValue = new Short((short) 10);
-		ShortValidatable<?> validatable = number(fieldName, fieldValue);
-		assertEquals(fieldName, validatable.getFieldName());
+		ShortValidatable<?> validatable = number(FIELD_NAME, fieldValue);
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void numberShortTest2() {
 		Short fieldValue = new Short((short) 10);
-		ShortValidatable<?> validatable = number(objectName, fieldName, fieldValue);
-		assertEquals(objectName, validatable.getObjectName());
-		assertEquals(fieldName, validatable.getFieldName());
+		ShortValidatable<?> validatable = number(OBJECT_NAME, FIELD_NAME, fieldValue);
+		assertEquals(OBJECT_NAME, validatable.getObjectName());
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void textTest() {
 		String fieldValue = "fieldValue";
-		StringValidatable<?> validatable = text(fieldName, fieldValue);
-		assertEquals(fieldName, validatable.getFieldName());
+		StringValidatable<?> validatable = text(FIELD_NAME, fieldValue);
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void textTest2() {
 		String fieldValue = "fieldValue";
-		StringValidatable<?> validatable = text(objectName, fieldName, fieldValue);
-		assertEquals(objectName, validatable.getObjectName());
-		assertEquals(fieldName, validatable.getFieldName());
+		StringValidatable<?> validatable = text(OBJECT_NAME, FIELD_NAME, fieldValue);
+		assertEquals(OBJECT_NAME, validatable.getObjectName());
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void dateTest() {
 		Date fieldValue = new Date();
-		DateValidatable<?> validatable = date(fieldName, fieldValue);
-		assertEquals(fieldName, validatable.getFieldName());
+		DateValidatable<?> validatable = date(FIELD_NAME, fieldValue);
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 
 	@Test
 	public void dateTest2() {
 		Date fieldValue = new Date();
-		DateValidatable<?> validatable = date(objectName, fieldName, fieldValue);
-		assertEquals(objectName, validatable.getObjectName());
-		assertEquals(fieldName, validatable.getFieldName());
+		DateValidatable<?> validatable = date(OBJECT_NAME, FIELD_NAME, fieldValue);
+		assertEquals(OBJECT_NAME, validatable.getObjectName());
+		assertEquals(FIELD_NAME, validatable.getFieldName());
 		assertEquals(fieldValue, validatable.getFieldValue());
 	}
 }
